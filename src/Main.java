209dcfd7c726b13c4d8b4e30e12c0eefe19b45d2
iskrot <olga.volwskaya@gmail.com>
2024-12-13 00:39:20 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
+        } else if (places >= 60 && places < 102) {
+            System.out.println("Осталось " + (102 - places) + " мест. Сидячих мест не осталось.");
+        } else {
+            System.out.println("Мест не осталось");
+        }
 
+        int one = 5;
+        int two = 2;
+        int three = 1;
+
+        if (one > two && one > three) {
+            System.out.println(one + " наибольшее число");
+        } else if (two > one && two > three) {
+            System.out.println(two + " наибольшее число");
+        } else {
+            System.out.println(three + " наибольшее число");
+        }
     }
 }
